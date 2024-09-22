0e92b0968067b3e5f691e8ab371525bedea15c67
James Turner <jturner@sugarlandtx.gov>
2024-09-22 00:39:57 -0500

    Condense api code

--- api/api.py
+++ api/api.py
@@ -1,11 +1,20 @@
 import io
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 import polars as pl
 import utils
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ===========================
 #  Read event code descriptors
@@ -13,8 +22,12 @@ app = FastAPI()
 #
 # ===========================
 
-# event codes
+# event codes with descriptions
 ec = pl.read_csv(source="api/event_codes.csv")
+# event code pairs
+ec_pairs = pl.read_csv("api/event_pairs.csv").rows()
+# single event codes
+ec_singles = pl.read_csv("api/event_singles.csv")["event_code"].to_list()
 
 
 @app.get("/purdue")
@@ -42,63 +55,11 @@ async def get_purdue(
     # ================================================
     #            Paired Event Code
     #
-    #  alarms that have seperate event codes for on/off
+    #  alarms that have paired event codes for on/off
     #
     # ================================================
 
-    ec_pairs = pl.read_csv("api/event_pairs.csv").rows()
-    eventdf_holder = []
-
-    for ec_pair in ec_pairs:
-
-        ec_params = df_data.filter(pl.col("event_code") == ec_pair[0])[
-            "parameter"
-        ].unique()
-
-        for param in ec_params:
-            # print(f"ec1: {ec_pair[0]}, ec2: {ec_pair[1]}: param: {param} ")
-
-            df_ec = df_data.filter(
-                pl.col("event_code").is_in([ec_pair[0], ec_pair[1]]),
-                pl.col("parameter") == param,
-            )
-            # df_ec.write_csv("df_ec1.csv")
-
-            df_ec = df_ec.filter(
-                pl.col("event_code")
-                != pl.col("event_code").shift(1, fill_value=ec_pair[1])
-            )
-
-            # df_ec.write_csv("df_ec2.csv")
-            # print(df_ec)
-
-            # Delete last row if ends with start pair
-            if df_ec["event_code"].item(df_ec.height - 1) == ec_pair[0]:
-                df_ec = df_ec.slice(0, df_ec.height - 1)
-
-            df_start = df_ec.filter(
-                pl.col("event_code") == ec_pair[0], pl.col("parameter") == param
-            )
-
-            df_end = df_ec.filter(
-                pl.col("event_code") == ec_pair[1], pl.col("parameter") == param
-            ).rename(lambda cname: cname + "2")
-
-            # If Event does not have pair, skip without matching
-            if df_start.is_empty() or df_end.is_empty():
-                print(f"ec1: {ec_pair[0]}, ec2: {ec_pair[1]}: param: {param} ")
-                continue
-
-            # print(df_start)
-            # print(df_end)
-
-            df_temp = (
-                df_start.hstack(df_end)
-                .with_columns(duration=pl.col("dt2") - pl.col("dt"))
-                .with_columns(pl.col("duration").dt.total_milliseconds() / 1000)
-            )
-
-            eventdf_holder.append(df_temp)
+    eventdf_holder = utils.pair_events(ec_pairs, df_data)
 
     # ================================================
     #                Single Event Code
@@ -108,27 +69,9 @@ async def get_purdue(
     #
     # ================================================
 
-    ec_singles = pl.read_csv("api/event_singles.csv")
-    ec_singles = ec_singles["event_code"].to_list()
+    df_singles = utils.single_events(ec_singles, df_data)
 
-    # for ec_single in ec_singles:
-
-    #     print(f"ec1: {ec_single[0]}, descriptor: {ec_single[1]} ")
-
-    df_es = df_data.filter(pl.col("event_code").is_in(ec_singles))
-    df_es2 = df_es.rename(lambda cname: cname + "2")
-
-    df_temp = (
-        df_es.hstack(df_es2)
-        # added .1 seconds to be able to display on timeline chart, for now leave off
-        # .with_columns(pl.col("dt2") + pl.duration(milliseconds=100))
-        .with_columns(duration=pl.col("dt2") - pl.col("dt")).with_columns(
-            pl.col("duration").dt.total_milliseconds() / 1000
-        )
-    )
-    # df_temp.write_csv("temp.csv")
-
-    eventdf_holder.append(df_temp)
+    eventdf_holder.append(df_singles)
 
     df_fin: pl.DataFrame = (
         pl.concat(eventdf_holder)
@@ -136,8 +79,70 @@ async def get_purdue(
         .select(pl.lit(locid).alias("loc_id"), pl.all())
     )
 
+    # df_fin.write_csv("api/test_results.csv")
+
     stream = io.StringIO()
     df_fin.write_csv(stream)
     response = StreamingResponse(iter([stream.getvalue()]), media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
     return response
+
+    # df_viz = df_fin.with_columns(
+    #     pl.col("dt").dt.timestamp("ms"), pl.col("dt2").dt.timestamp("ms")
+    # )
+
+    # series = []
+    # colors = []
+
+    # ring_ec = {
+    #     "Green": (1, 7, "#00E396"),
+    #     "Yellow Clr": (8, 9, "#FEB019"),
+    #     "Red Clr": (10, 11, "#FF4560"),
+    # }
+
+    # phases = [
+    #     (1, "R1"),
+    #     (2, "R1"),
+    #     (3, "R1"),
+    #     (4, "R1"),
+    #     (5, "R2"),
+    #     (6, "R2"),
+    #     (7, "R2"),
+    #     (8, "R2"),
+    # ]
+    # num_records = 0
+    # for k, v in ring_ec.items():
+
+    #     for phase in phases:
+    #         # TODO: do i have to convert time to milliseconds?
+    #         df = df_viz.filter(
+    #             # pl.col("event_code2").is_in([7, 9, 11]) & pl.col("parameter2").is_in([1, 2, 3, 4])
+    #             (pl.col("event_code") == v[0])
+    #             & (pl.col("event_code2") == v[1])
+    #             & (pl.col("parameter") == phase[0])
+    #         )
+    #         # df.write_csv("api/test_results.csv")
+
+    #         df = df.with_columns(
+    #             y=pl.concat_list("dt", "dt2"), x=pl.lit(phase[1])
+    #         ).select(["x", "y"])
+
+    #         # df.write_json("api/test_results.json")
+
+    #         # TODO: how do i add the phase here to name without another nested for loop?
+    #         temp_d = {"name": f"Ph {phase[0]} {k}"}
+    #         num_records += len(df)
+    #         temp_d["data"] = df.to_dicts()
+    #         series.append(temp_d)
+    #         colors.append(v[2])
+
+    # # with open("api/res.json", "w") as f:
+    # #     for items in series:
+    # #         f.write("%s\n" % items)
+    # print(num_records)
+    # # temp = [series, colors]
+    # # print(series)\
+    # res = dict()
+    # res["series"] = series
+    # res["colors"] = colors
+    # return res
--- api/utils.py
+++ api/utils.py
@@ -70,3 +70,79 @@ def add_event_descriptors(dir_list: list, path: str):
         df_holder.append(df)
 
     return df_holder
+
+
+def pair_events(ec_pairs, df_data) -> list[pl.DataFrame]:
+
+    eventdf_holder = []
+
+    for ec_pair in ec_pairs:
+
+        ec_params = df_data.filter(pl.col("event_code") == ec_pair[0])[
+            "parameter"
+        ].unique()
+
+        for param in ec_params:
+            # print(f"ec1: {ec_pair[0]}, ec2: {ec_pair[1]}: param: {param} ")
+
+            df_ec = df_data.filter(
+                pl.col("event_code").is_in([ec_pair[0], ec_pair[1]]),
+                pl.col("parameter") == param,
+            )
+            # df_ec.write_csv("df_ec1.csv")
+
+            df_ec = df_ec.filter(
+                pl.col("event_code")
+                != pl.col("event_code").shift(1, fill_value=ec_pair[1])
+            )
+
+            # df_ec.write_csv("df_ec2.csv")
+            # print(df_ec)
+
+            # Delete last row if ends with start pair
+            if df_ec["event_code"].item(df_ec.height - 1) == ec_pair[0]:
+                df_ec = df_ec.slice(0, df_ec.height - 1)
+
+            df_start = df_ec.filter(
+                pl.col("event_code") == ec_pair[0], pl.col("parameter") == param
+            )
+
+            df_end = df_ec.filter(
+                pl.col("event_code") == ec_pair[1], pl.col("parameter") == param
+            ).rename(lambda cname: cname + "2")
+
+            # If Event does not have pair, skip without matching
+            if df_start.is_empty() or df_end.is_empty():
+                print(f"ec1: {ec_pair[0]}, ec2: {ec_pair[1]}: param: {param} ")
+                continue
+
+            # print(df_start)
+            # print(df_end)
+
+            df_temp = (
+                df_start.hstack(df_end)
+                .with_columns(duration=pl.col("dt2") - pl.col("dt"))
+                .with_columns(pl.col("duration").dt.total_milliseconds() / 1000)
+            )
+
+            eventdf_holder.append(df_temp)
+
+    return eventdf_holder
+
+
+def single_events(ec_singles: list, df_data) -> pl.DataFrame:
+
+    df_es = df_data.filter(pl.col("event_code").is_in(ec_singles))
+    df_es2 = df_es.rename(lambda cname: cname + "2")
+
+    df_singles = (
+        df_es.hstack(df_es2)
+        # added .1 seconds to be able to display on timeline chart, for now leave off
+        # .with_columns(pl.col("dt2") + pl.duration(milliseconds=100))
+        .with_columns(duration=pl.col("dt2") - pl.col("dt")).with_columns(
+            pl.col("duration").dt.total_milliseconds() / 1000
+        )
+    )
+    # df_temp.write_csv("temp.csv")
+
+    return df_singles
