db8858509dbef596c125d26878ecf907f388c457
James Turner <jturner@sugarlandtx.gov>
2024-09-28 07:39:56 -0500

    org api code better

--- api/api.py
+++ api/api.py
@@ -1,10 +1,12 @@
 import io
+import os
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import polars as pl
 import utils
 
+uri = os.getenv("URI")
 app = FastAPI()
 
 
@@ -16,10 +18,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 # ===========================
-#  Read event code descriptors
-#
-#
+#  * Load Event code data
 # ===========================
 
 # event codes with descriptions
@@ -29,11 +30,35 @@ ec_pairs = pl.read_csv("api/event_pairs.csv").rows()
 # single event codes
 ec_singles = pl.read_csv("api/event_singles.csv")["event_code"].to_list()
 
+# ================================================
+# *             Locations
+# Used to populate locations dropdown
+# ================================================
 
-@app.get("/purdue")
-async def get_purdue(
+
+@app.get("/form_locids")
+async def get_locations() -> list[dict]:
+    """Return list of atms_id, location name dictionaries
+    for select dropdown"""
+
+    qry = """SELECT atms_id, name FROM intersection"""
+    df = pl.read_database_uri(query=qry, uri=uri, engine="adbc")
+
+    # NOTE: anotther way to acheive results
+    # w/o list, only dictionary
+    # df = dict(zip(df["atms_id"], df["name"]))
+    return df.to_dicts()
+
+
+# ================================================
+# *               Hi-res Function
+# get and process hi-res data into dataframe
+# ================================================
+
+
+def process_hires(
     locid: str, date: str, time: str | None = None, addhrs: int | None = None
-) -> StreamingResponse:
+) -> pl.DataFrame:
 
     # retun filtered list of files from directory
     dir_list, path = utils.filter_directory(locid, date, time, addhrs)
@@ -53,20 +78,14 @@ async def get_purdue(
     )
 
     # ================================================
-    #            Paired Event Code
-    #
+    # *             Pair Event Code
     #  alarms that have paired event codes for on/off
-    #
     # ================================================
-
     eventdf_holder = utils.pair_events(ec_pairs, df_data)
 
     # ================================================
-    #                Single Event Code
-    #
-    #
+    #  *             Single Event Code
     #   Events that only have singel event code
-    #
     # ================================================
 
     df_singles = utils.single_events(ec_singles, df_data)
@@ -81,63 +100,36 @@ async def get_purdue(
 
     # df_fin.write_csv("api/test_results.csv")
 
+    return df_fin
+
+
+@app.get("/purdue")
+async def get_purdue(
+    locid: str, date: str, time: str | None = None, addhrs: int | None = None
+) -> StreamingResponse:
+
+    df_hres = process_hires(locid=locid, date=date, time=time, addhrs=addhrs)
+
     stream = io.StringIO()
-    df_fin.write_csv(stream)
+    df_hres.write_csv(stream)
     response = StreamingResponse(iter([stream.getvalue()]), media_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=export.csv"
     return response
 
 
+# ================================================
+# *        Endpoint for HiRes Timeline
+# ================================================
+
+
 @app.get("/timeline_viz")
 async def get_purdue(
     locid: str, date: str, time: str | None = None, addhrs: int | None = None
-) -> StreamingResponse:
+) -> dict[dict]:
 
-    # retun filtered list of files from directory
-    dir_list, path = utils.filter_directory(locid, date, time, addhrs)
+    df_hres = process_hires(locid=locid, date=date, time=time, addhrs=addhrs)
 
-    # read files and add event descriptors
-    df_holder: list[pl.DataFrame] = utils.add_event_descriptors(dir_list, path)
-
-    # Concat list of dataframes to one dataframe for processing
-    df_data: pl.DataFrame = (
-        pl.concat(df_holder).sort(by="dt")
-        # Use series to map values from df to another df, great feature!!
-        .with_columns(
-            event_descriptor=pl.col("event_code").replace_strict(
-                old=ec["event_code"], new=ec["event_descriptor"], default="x"
-            )
-        )
-    )
-
-    # ================================================
-    #            Paired Event Code
-    #
-    #  alarms that have paired event codes for on/off
-    #
-    # ================================================
-
-    eventdf_holder = utils.pair_events(ec_pairs, df_data)
-
-    # ================================================
-    #                Single Event Code
-    #
-    #
-    #   Events that only have singel event code
-    #
-    # ================================================
-
-    df_singles = utils.single_events(ec_singles, df_data)
-
-    eventdf_holder.append(df_singles)
-
-    df_fin: pl.DataFrame = (
-        pl.concat(eventdf_holder)
-        .sort(by="dt")
-        .select(pl.lit(locid).alias("loc_id"), pl.all())
-    )
-
-    df_viz = df_fin.with_columns(
+    df_viz = df_hres.with_columns(
         pl.col("dt").dt.timestamp("ms"), pl.col("dt2").dt.timestamp("ms")
     )
 
@@ -163,10 +155,9 @@ async def get_purdue(
     num_records = 0
     for k, v in ring_ec.items():
 
+        # TODO: how do i add the phase here to name without another nested for loop?
         for phase in phases:
-            # TODO: do i have to convert time to milliseconds?
             df = df_viz.filter(
-                # pl.col("event_code2").is_in([7, 9, 11]) & pl.col("parameter2").is_in([1, 2, 3, 4])
                 (pl.col("event_code") == v[0])
                 & (pl.col("event_code2") == v[1])
                 & (pl.col("parameter") == phase[0])
@@ -179,7 +170,6 @@ async def get_purdue(
 
             # df.write_json("api/test_results.json")
 
-            # TODO: how do i add the phase here to name without another nested for loop?
             temp_d = {"name": f"Ph {phase[0]} {k}"}
             num_records += len(df)
             temp_d["data"] = df.to_dicts()
@@ -190,9 +180,21 @@ async def get_purdue(
     #     for items in series:
     #         f.write("%s\n" % items)
     print(num_records)
-    # temp = [series, colors]
-    # print(series)\
+
     res = dict()
     res["series"] = series
     res["colors"] = colors
     return res
+
+
+# ================================================
+# *            Hi-res AG grid endpoint
+# Populate hi-res data in ag grid
+# ================================================
+
+
+@app.get("/hiresgrid")
+async def get_hires_grid(
+    locid: str, date: str, time: str | None = None, addhrs: int | None = None
+) -> list[dict]:
+    pass
